b653c1bbfcbcdb9348effd228ea1797fef61b253
Steve Smith <steve@kentsmiths.com>
2020-02-10 09:00:20 -0500

    Adding XML comments and disabling package on build

--- src/Ardalis.ApiEndpoints/BaseEndpoint.cs
+++ src/Ardalis.ApiEndpoints/BaseEndpoint.cs
@@ -2,12 +2,21 @@
 
 namespace Ardalis.ApiEndpoints
 {
+    /// <summary>
+    /// A base class for an endpoint that accepts parameters.
+    /// </summary>
+    /// <typeparam name="TRequest"></typeparam>
+    /// <typeparam name="TResponse"></typeparam>
     [ApiController]
     public abstract class BaseEndpoint<TRequest, TResponse> : ControllerBase
     {
         public abstract ActionResult<TResponse> Handle(TRequest request);
     }
 
+    /// <summary>
+    /// A base calss for an endpoint that has no parameters.
+    /// </summary>
+    /// <typeparam name="TResponse"></typeparam>
     [ApiController]
     public abstract class BaseEndpoint<TResponse> : ControllerBase
     {
