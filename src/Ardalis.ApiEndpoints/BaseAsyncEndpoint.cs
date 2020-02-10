8bced78c3a6c3731254603dfd582b61dc7caa26f
Steve Smith <steve@kentsmiths.com>
2020-02-10 10:46:39 -0500

    async support (#8)
    * Update package icon
    
    * Add async endpoint types
    
    * Added async support
    
    * Update version for NuGet

---
+++ src/Ardalis.ApiEndpoints/BaseAsyncEndpoint.cs
@@ -0,0 +1,26 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System.Threading.Tasks;
+
+namespace Ardalis.ApiEndpoints
+{
+    /// <summary>
+    /// A base class for an endpoint that accepts parameters.
+    /// </summary>
+    /// <typeparam name="TRequest"></typeparam>
+    /// <typeparam name="TResponse"></typeparam>
+    [ApiController]
+    public abstract class BaseAsyncEndpoint<TRequest, TResponse> : ControllerBase
+    {
+        public abstract Task<ActionResult<TResponse>> HandleAsync(TRequest request);
+    }
+
+    /// <summary>
+    /// A base calss for an endpoint that has no parameters.
+    /// </summary>
+    /// <typeparam name="TResponse"></typeparam>
+    [ApiController]
+    public abstract class BaseAsyncEndpoint<TResponse> : ControllerBase
+    {
+        public abstract Task<ActionResult<TResponse>> HandleAsync();
+    }
+}
