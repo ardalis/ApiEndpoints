4161e04ab9498e024e071092daf53043d43f9413
Steve Smith <steve@kentsmiths.com>
2020-02-10 10:20:12 -0500

    Add async endpoint types

--- src/Ardalis.ApiEndpoints/BaseEndpoint.cs
+++ src/Ardalis.ApiEndpoints/BaseEndpoint.cs
@@ -5,8 +5,8 @@ namespace Ardalis.ApiEndpoints
     /// <summary>
     /// A base class for an endpoint that accepts parameters.
     /// </summary>
-    /// <typeparam name="TRequest"></typeparam>
-    /// <typeparam name="TResponse"></typeparam>
+    /// <typeparam name="TRequest">The type to be model bound by the Handle method.</typeparam>
+    /// <typeparam name="TResponse">The type to return from the Handle method's ActionResult<T>.</typeparam>
     [ApiController]
     public abstract class BaseEndpoint<TRequest, TResponse> : ControllerBase
     {
@@ -16,8 +16,7 @@ namespace Ardalis.ApiEndpoints
     /// <summary>
     /// A base calss for an endpoint that has no parameters.
     /// </summary>
-    /// <typeparam name="TResponse"></typeparam>
-    [ApiController]
+    /// <typeparam name="TResponse">The type to return from the Handle method's ActionResult<T>.</    [ApiController]
     public abstract class BaseEndpoint<TResponse> : ControllerBase
     {
         public abstract ActionResult<TResponse> Handle();
