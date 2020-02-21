7c2b464bfb09c8057d3f96bc9ad596f55bbcd94b
Kyle McMaster <KyleMcMaster@users.noreply.github.com>
2020-02-21 10:19:50 -0500

    Add Authors Functional Tests (#11)
    * feat(tests): initial implementation of functional tests
    
    * feat(tests): remove create, update, and delete functional tests, update get and list functional test naming

---
+++ sample/Sample.FunctionalTests/AuthorEndpoints/GetEndpoint.cs
@@ -0,0 +1,38 @@
+using Newtonsoft.Json;
+using SampleEndpointApp;
+using SampleEndpointApp.DataAccess;
+using SampleEndpointApp.DomainModel;
+using System.Linq;
+using System.Net.Http;
+using System.Threading.Tasks;
+using Xunit;
+
+namespace Sample.FunctionalTests.AuthorEndpoints
+{
+    public class GetEndpoint : IClassFixture<CustomWebApplicationFactory<Startup>>
+    {
+        private readonly HttpClient _client;
+
+        public GetEndpoint(CustomWebApplicationFactory<Startup> factory)
+        {
+            _client = factory.CreateClient();
+        }
+
+        [Fact]
+        public async Task ReturnsAuthorById()
+        {
+            var firstAuthor = SeedData.Authors().First();
+
+            var response = await _client.GetAsync($"/authors/{firstAuthor.Id}");
+            response.EnsureSuccessStatusCode();
+            var stringResponse = await response.Content.ReadAsStringAsync();
+            var result = JsonConvert.DeserializeObject<Author>(stringResponse);
+
+            Assert.NotNull(result);
+            Assert.Equal(firstAuthor.Id, result.Id);
+            Assert.Equal(firstAuthor.Name, result.Name);
+            Assert.Equal(firstAuthor.PluralsightUrl, result.PluralsightUrl);
+            Assert.Equal(firstAuthor.TwitterAlias, result.TwitterAlias);
+        }
+    }
+}
---
+++ sample/Sample.FunctionalTests/CustomWebApplicationFactory.cs
@@ -0,0 +1,64 @@
+﻿using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Mvc.Testing;
+using Microsoft.AspNetCore.TestHost;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Logging;
+using SampleEndpointApp;
+using SampleEndpointApp.DataAccess;
+using System;
+
+namespace Sample.FunctionalTests
+{
+    public class CustomWebApplicationFactory<TStartup> : WebApplicationFactory<Startup>
+    {
+        protected override void ConfigureWebHost(IWebHostBuilder builder)
+        {
+            builder
+                .UseSolutionRelativeContentRoot("sample/SampleEndpointApp")
+                .ConfigureServices(services =>
+                {
+                    // Create a new service provider.
+                    var serviceProvider = new ServiceCollection()
+                        .AddEntityFrameworkInMemoryDatabase()
+                        .BuildServiceProvider();
+
+                    // Add a database context (AppDbContext) using an in-memory
+                    // database for testing.
+                    services.AddDbContext<AppDbContext>(options =>
+                    {
+                        options.UseInMemoryDatabase($"InMemoryDbForTesting");
+                        options.UseInternalServiceProvider(serviceProvider);
+                    });
+
+                    // Build the service provider.
+                    var sp = services.BuildServiceProvider();
+
+                    // Create a scope to obtain a reference to the database
+                    // context (AppDbContext).
+                    using (var scope = sp.CreateScope())
+                    {
+                        var scopedServices = scope.ServiceProvider;
+                        var db = scopedServices.GetRequiredService<AppDbContext>();
+
+                        var logger = scopedServices
+                            .GetRequiredService<ILogger<CustomWebApplicationFactory<TStartup>>>();
+
+                        // Ensure the database is created.
+                        db.Database.EnsureCreated();
+
+                        try
+                        {
+                            // Seed the database with test data.
+                            SeedData.PopulateTestData(db);
+                        }
+                        catch (Exception ex)
+                        {
+                            logger.LogError(ex, "An error occurred seeding the " +
+                                $"database with test messages. Error: {ex.Message}");
+                        }
+                    }
+                });
+        }
+    }
+}
