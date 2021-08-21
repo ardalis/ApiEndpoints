41e49ee35a54bc9520ebc54229d3d8a106438d60
Maksym Koshovyi <maximkoshevoi61@gmail.com>
2021-08-22 00:33:57 +0300

    Add route source generator

---
+++ Ardalis.ApiEndpoints.CodeGenerators/ChildClassReceiver.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections.Generic;
+using Microsoft.CodeAnalysis;
+using Microsoft.CodeAnalysis.CSharp.Syntax;
+
+namespace Ardalis.ApiEndpoints.CodeGenerators
+{
+    public sealed class ChildClassReceiver : ISyntaxReceiver
+    {
+        private readonly List<ClassDeclarationSyntax> _candidates = new();
+
+        public IReadOnlyList<ClassDeclarationSyntax> Candidates => _candidates;
+
+        public void OnVisitSyntaxNode(SyntaxNode syntaxNode)
+        {
+            if (syntaxNode is ClassDeclarationSyntax classSyntax && classSyntax.BaseList?.Types.Any() == true)
+            {
+                _candidates.Add(classSyntax);
+            }
+        }
+    }
+}
---
+++ Ardalis.ApiEndpoints.CodeGenerators/RouteGenerator.cs
@@ -0,0 +1,44 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+using Microsoft.CodeAnalysis;
+
+namespace Ardalis.ApiEndpoints.CodeGenerators
+{
+    [Generator]
+    public class RouteGenerator : ISourceGenerator
+    {
+        public void Initialize(GeneratorInitializationContext context) 
+        {
+            context.RegisterForSyntaxNotifications(() => new ChildClassReceiver());
+        }
+
+        public void Execute(GeneratorExecutionContext context)
+        {
+            if (context.SyntaxReceiver is not ChildClassReceiver baseEndpointReceiver)
+            {
+                return;
+            }
+
+            List<ITypeSymbol> baseEndpointChildren = baseEndpointReceiver.Candidates
+                .Select(c => context.Compilation.GetSemanticModel(c.SyntaxTree).GetDeclaredSymbol(c))
+                .OfType<ITypeSymbol>()
+                .Where(s => s.GetBaseTypesAndThis().Any(t => t.ToString() == "Ardalis.ApiEndpoints.BaseEndpoint"))
+                .ToList();
+
+            foreach (ITypeSymbol targetType in baseEndpointChildren)
+            {
+                string sourceCode = GenerateRoute(targetType);
+                context.AddSource($"{targetType.Name}.Route.cs", sourceCode);
+            }
+        }
+
+        private string GenerateRoute(ITypeSymbol targetType) =>
+$@"using Microsoft.AspNetCore.Mvc;
+
+namespace {targetType.ContainingNamespace}
+{{
+    [Route(""api/{targetType.ContainingNamespace.Name}"")]
+    partial class {targetType.Name} {{ }}
+}}";
+    }
+}
