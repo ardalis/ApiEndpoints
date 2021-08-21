d07a4d95501e13ff1df013ace5fc6ca69186487c
Maksym Koshovyi <maximkoshevoi61@gmail.com>
2021-08-21 22:16:50 +0300

    Update implementation

--- src/Ardalis.ApiEndpoints/BaseEndpoint.cs
+++ src/Ardalis.ApiEndpoints/BaseEndpoint.cs
@@ -3,44 +3,10 @@
 namespace Ardalis.ApiEndpoints
 {
     /// <summary>
-    /// A base class for an endpoint that accepts parameters.
-    /// </summary>
-    /// <typeparam name="TRequest"></typeparam>
-    /// <typeparam name="TResponse"></typeparam>
-    public static class BaseEndpoint
-    {
-        public static class WithRequest<TRequest>
-        {
-            public abstract class WithResponse<TResponse> : BaseEndpointSync
-            {
-                public abstract ActionResult<TResponse> Handle(TRequest request);
-            }
-
-            public abstract class WithoutResponse : BaseEndpointSync
-            {
-                public abstract ActionResult Handle(TRequest request);
-            }
-        }
-
-        public static class WithoutRequest
-        {
-            public abstract class WithResponse<TResponse> : BaseEndpointSync
-            {
-                public abstract ActionResult<TResponse> Handle();
-            }
-
-            public abstract class WithoutResponse : BaseEndpointSync
-            {
-                public abstract ActionResult Handle();
-            }
-        }
-    }
-
-    /// <summary>
-    /// A base class for all synchronous endpoints.
+    /// A base class for all endpoints.
     /// </summary>
 	[ApiController]
-    public abstract class BaseEndpointSync : ControllerBase
+    public abstract class BaseEndpoint : ControllerBase
     {
     }
 }
