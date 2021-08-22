079ddc984d307e0b5f4e797c74516ce258a4f305
Maksym Koshovyi <maximkoshevoi61@gmail.com>
2021-08-22 15:15:11 +0300

    Create Ardalis.ApiEndpoints.NSwag project

---
+++ src/ApiEndpoints.NSwag/EndpointOperationProcessor.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using Ardalis.ApiEndpoints;
+using NSwag.Generation.Processors;
+using NSwag.Generation.Processors.Contexts;
+
+namespace ApiEndpoints.NSwag
+{
+    internal class EndpointOperationProcessor : IOperationProcessor
+    {
+        public bool Process(OperationProcessorContext context)
+        {
+            if (context.ControllerType.GetBaseTypesAndThis().Any(t => t == typeof(EndpointBase)))
+            {
+                context.OperationDescription.Operation.Tags = new List<string> { context.ControllerType.Namespace.Split('.').Last() };
+            }
+
+            return true;
+        }
+    }
+}
