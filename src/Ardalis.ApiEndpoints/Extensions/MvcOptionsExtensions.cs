d825c17a1c9e1104dc622938560688f6645eb6b1
Maksym Koshovyi <maximkoshevoi61@gmail.com>
2021-08-24 23:53:08 +0300

    More general implementation

--- src/Ardalis.ApiEndpoints/Extensions/MvcOptionsExtensions.cs
+++ src/Ardalis.ApiEndpoints/Extensions/MvcOptionsExtensions.cs
@@ -1,4 +1,5 @@
-﻿using System.Collections.Generic;
+﻿using System;
+using System.Collections.Generic;
 using System.Linq;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.ApplicationModels;
@@ -9,27 +10,39 @@ namespace Microsoft.Extensions.DependencyInjection
     {
         public static void UseNamespaceRouteToken(this MvcOptions options)
         {
-            options.Conventions.Add(new NamespaceRoutingConvention());
+            options.Conventions.Add(new CustomRouteToken(
+                "namespace",
+                c => c.ControllerType.Namespace?.Split('.').Last() ?? string.Empty)
+            );
         }
 
-        private class NamespaceRoutingConvention : IApplicationModelConvention
+        private class CustomRouteToken : IApplicationModelConvention
         {
+            private readonly string _tokenName;
+            private readonly Func<ControllerModel, string> _valueGenerator;
+
+            public CustomRouteToken(string tokenName, Func<ControllerModel, string> valueGenerator)
+            {
+                _tokenName = $"[{tokenName}]";
+                _valueGenerator = valueGenerator;
+            }
+
             public void Apply(ApplicationModel application)
             {
                 foreach (var controller in application.Controllers)
                 {
-                    string namespaceValue = controller.ControllerType.Namespace?.Split('.').Last() ?? string.Empty;
-                    UpdateSelectors(controller.Selectors, namespaceValue);
-                    UpdateSelectors(controller.Actions.SelectMany(a => a.Selectors), namespaceValue);
+                    string tokenValue = _valueGenerator(controller);
+                    UpdateSelectors(controller.Selectors, tokenValue);
+                    UpdateSelectors(controller.Actions.SelectMany(a => a.Selectors), tokenValue);
                 }
             }
 
-            private static void UpdateSelectors(IEnumerable<SelectorModel> selectors, string namespaceValue)
+            private void UpdateSelectors(IEnumerable<SelectorModel> selectors, string tokenValue)
             {
                 foreach (var selector in selectors.Where(s => s.AttributeRouteModel != null))
                 {
                     selector.AttributeRouteModel.Template = selector.AttributeRouteModel.Template
-                        .Replace("[namespace]", namespaceValue);
+                        .Replace(_tokenName, tokenValue);
                 }
             }
         }
