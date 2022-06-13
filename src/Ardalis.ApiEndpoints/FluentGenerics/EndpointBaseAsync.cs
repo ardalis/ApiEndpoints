09a1e260ae28957eb5324604fa2dc0638daf3085
Steve Smith <steve@kentsmiths.com>
2022-06-12 23:27:42 -0400

    updating package settings and adding icon files

--- src/Ardalis.ApiEndpoints/FluentGenerics/EndpointBaseAsync.cs
+++ src/Ardalis.ApiEndpoints/FluentGenerics/EndpointBaseAsync.cs
@@ -42,6 +42,42 @@ public static class EndpointBaseAsync
     }
   }
 
+  public static class WithIdAndRequest<TId,TRequest>
+  {
+    public abstract class WithResult<TResponse> : EndpointBase
+    {
+      public abstract Task<TResponse> HandleAsync(
+          TId id, TRequest request,
+          CancellationToken cancellationToken = default
+      );
+    }
+
+    public abstract class WithoutResult : EndpointBase
+    {
+      public abstract Task HandleAsync(
+          TId id, TRequest request,
+          CancellationToken cancellationToken = default
+      );
+    }
+
+    public abstract class WithActionResult<TResponse> : EndpointBase
+    {
+      public abstract Task<ActionResult<TResponse>> HandleAsync(
+          TId id, TRequest request,
+          CancellationToken cancellationToken = default
+      );
+    }
+
+    public abstract class WithActionResult : EndpointBase
+    {
+      public abstract Task<ActionResult> HandleAsync(
+          TId id, TRequest request,
+          CancellationToken cancellationToken = default
+      );
+    }
+  }
+
+
   public static class WithoutRequest
   {
     public abstract class WithResult<TResponse> : EndpointBase
