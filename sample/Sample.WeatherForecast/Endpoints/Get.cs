536d9037730495444788f2d8aa656d6136a5f638
ali zaferany <ali.zaferany79@gmail.com>
2022-09-03 13:29:34 +0430

    perf: add WeatherForecast template

---
+++ sample/Sample.WeatherForecast/Endpoints/Get.cs
@@ -0,0 +1,39 @@
+﻿using Ardalis.ApiEndpoints;
+using Microsoft.AspNetCore.Mvc;
+using Swashbuckle.AspNetCore.Annotations;
+
+namespace Sample.WeatherForecast.Endpoints;
+
+public class Get : EndpointBaseSync
+  .WithoutRequest
+  .WithActionResult<IEnumerable<WeatherForecast>>
+{
+  private static readonly string[] Summaries = new[]
+  {
+    "Freezing", "Bracing", "Chilly", "Cool", "Mild", "Warm", "Balmy", "Hot", "Sweltering", "Scorching"
+  };
+
+  private readonly ILogger<Get> _logger;
+
+  public Get(ILogger<Get> logger)
+  {
+    _logger = logger;
+  }
+
+  [HttpGet("/WeatherForecast")]
+  [SwaggerOperation(
+    Summary = "Get weather forecast",
+    OperationId = "WeatherForecast.Get",
+    Tags = new[] { "WeatherForecast" })
+  ]
+  public override ActionResult<IEnumerable<WeatherForecast>> Handle()
+  {
+    return Enumerable.Range(1, 5).Select(index => new WeatherForecast
+                     {
+                       Date = DateTime.Now.AddDays(index),
+                       TemperatureC = Random.Shared.Next(-20, 55),
+                       Summary = Summaries[Random.Shared.Next(Summaries.Length)]
+                     })
+                     .ToArray();
+  }
+}
